8e1386902f22ff4248a355227c0b4c2284cfc0ac
MohithS <mohithgowda5453@gmail.com>
2026-04-01 12:02:52 +0530

    day 3

--- src/com/ibm/demo/day3/multithreading/MultiThreadingDemo.java
+++ src/com/ibm/demo/day3/multithreading/MultiThreadingDemo.java
@@ -1,10 +1,34 @@
 package com.ibm.demo.day3.multithreading;
 
 public class MultiThreadingDemo {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Start");
+		Thread obj1 = new Thread(new ThreadsDemo());
+		obj1.start();
+		Thread obj2 = new Thread(new ThreadsDemo());
+		obj2.start();
+		Thread obj3 = new Thread(new ThreadsDemo());
+		obj3.start();
+		System.out.println("End");
 	}
-
 }
+
+//package com.ibm.demo.day3.multithreading;
+//
+//public class MultiThreadingDemo {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//		System.out.println("Start");
+//		ThreadsDemo obj1 = new ThreadsDemo();
+//		obj1.printNums();
+//		ThreadsDemo obj2 = new ThreadsDemo();
+//		obj2.printNums();
+//		ThreadsDemo obj3 = new ThreadsDemo();
+//		obj3.printNums();
+//		System.out.println("End");
+//	}
+//
+//}
--- src/com/ibm/demo/day3/multithreading/ThreadsDemo.java
+++ src/com/ibm/demo/day3/multithreading/ThreadsDemo.java
@@ -1,5 +1,37 @@
 package com.ibm.demo.day3.multithreading;
 
-public class ThreadsDemo {
+public class ThreadsDemo extends Thread {
 
+	void printNums() {
+		for (int i = 1; i <= 10; i++) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+
+	@Override
+	public void run() {
+		this.printNums();
+	}
 }
+
+
+//package com.ibm.demo.day3.multithreading;
+//
+//public class ThreadsDemo {
+//
+//	void printNums() {
+//		for (int i = 1; i <= 10; i++) {
+//			try {
+//				Thread.sleep(250);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(i);
+//		}
+//	}
+//}
